e2e6039b70761a20228515c156aee2888ff86b02
echoshihab <echoshihab@gmail.com>
2021-10-31 20:03:56 -0400

    add seed data functionality

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());    
+            }
+
+        }
+
+        private static void SeedData(AppDbContext dbContext)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            if(!dbContext.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+                dbContext.Platforms.AddRange(
+                    new Platform() {Name = "Dot Net", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform() {Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform() {Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"}
+
+                );
+
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("--> we already have data");
+            }
+
+        }
+    }
+}
