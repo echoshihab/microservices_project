945dcc0b62a2c56187a168f20f3814f319415cac
echoshihab <echoshihab@gmail.com>
2021-12-31 13:15:39 -0500

    add GenericEvent and PlatformPublished Dtos

---
+++ CommandService/Dtos/PlatformPublishedDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Dtos
+{
+    public class PlatformPublishedDto
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Event {get;set;}
+        
+    }
+}
